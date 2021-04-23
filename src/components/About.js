d4e284e77bc51bd3b2d3bf3b475d8ffab9e1d91b
Vishal Khandate <vishukblog000@gmail.com>
2021-04-23 23:50:16 +0530

    Redirect Included

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+// import { Redirect } from "react-router-dom";
 
 function About() {
   // const [logout, setLogout] = useState(false);
   return (
     <div className="component about">
       <h1>About</h1>
+
+      {/* <button
+        onClick={() => {
+          console.log("redirecting to '/' ");
+          setLogout(true);
+        }}
+      >
+        Logout
+      </button>
+      {logout && <Redirect to="/" />} */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import { Nav, Navbar } from "react-bootstrap";
-function MainNavbar() {
-  // const [logout, setLogout] = useState(false);
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>React Router</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/about">About</Nav.Link>
-        <Nav.Link href="/users">Users</Nav.Link>
-      </Nav>
-    </Navbar>
-  );
+
+class MainNavbar extends Component {
+  state = {
+    logout: false,
+  };
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>React Router</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/users">Users</Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
 }
 
 export default MainNavbar;
